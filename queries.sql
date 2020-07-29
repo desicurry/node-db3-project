466f59847249934a25f1113b71d9fb87a54a342e
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-07-28 20:54:57 -0400

    MVP + Stretch

--- queries.sql
+++ queries.sql
@@ -1,36 +1,49 @@
--- Multi-Table Query Practice
+-- -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+-- -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
-select Category.CategoryName, Product.ProductName
-from Product
-join Category
-on Product.CategoryId=Category.Id
+-- select Category.CategoryName, Product.ProductName
+-- from Product
+-- join Category
+-- on Product.CategoryId=Category.Id
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+-- -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-select [Order].id, Shipper.CompanyName
-from [Order]
-join Shipper
-on [Order].ShipVia = Shipper.Id
-where [Order].OrderDate < '2012-08-09';
+-- select [Order].id, Shipper.CompanyName
+-- from [Order]
+-- join Shipper
+-- on [Order].ShipVia = Shipper.Id
+-- where [Order].OrderDate < '2012-08-09';
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-select Product.ProductName, OrderDetail.Quantity
-from [Order] 
-join OrderDetail 
-on [Order].id=OrderDetail.OrderId
-join Product 
-on OrderDetail.ProductId=Product.Id
-where [Order].Id=10251
-order by Product.ProductName
+-- select Product.ProductName, OrderDetail.Quantity
+-- from [Order] 
+-- join OrderDetail 
+-- on [Order].id=OrderDetail.OrderId
+-- join Product 
+-- on OrderDetail.ProductId=Product.Id
+-- where [Order].Id=10251
+-- order by Product.ProductName
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
-select [Order].Id, Customer.CompanyName, Employee.LastName
-from [Order]
-join Customer
-on [Order].CustomerId=Customer.Id
-join Employee
-on [Order].EmployeeId=Employee.id
+-- select [Order].Id, Customer.CompanyName, Employee.LastName
+-- from [Order]
+-- join Customer
+-- on [Order].CustomerId=Customer.Id
+-- join Employee
+-- on [Order].EmployeeId=Employee.id
+
+-- -- STRETCH PROBLEMS
+
+-- --   Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 8 records.
+-- SELECT Categories.CategoryName as "Category", count(*) as "Total Products"
+-- FROM Products
+-- JOIN Categories
+-- WHERE Products.CategoryID = Categories.CategoryID
+-- GROUP BY Products.CategoryID
+
+-- --   Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+-- SELECT OrderID, count(*) as "ItemCount" from OrderDetails
+-- GROUP BY OrderID 
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,51 +1,57 @@
-const db = require("../data/db-config");
-
-function find() {
-  return db("schemes");
-}
-function findById(id) {
-  return db("schemes").where({ id }).first();
-}
-function getStepId(id) {
-  return db("steps").where({ id }).first();
-}
-function findSteps(scheme_id) {
-  return db("steps as st")
-    .join("schemes as sc", "st.scheme_id", "sc.id")
-    .select("st.instructions", "st.step_number")
-    .where({ scheme_id });
-}
-function add(scheme) {
-  return db("schemes")
-    .insert(scheme)
-    .then((ids) => {
-      return findById(ids[0]);
-    });
-}
-function addStep(step) {
-  return db("steps")
-    .insert(step)
-    .then((ids) => {
-      return getStepId(ids[0]);
-    });
-}
-function update(changes, id) {
-  return db("schemes")
-    .where({ id })
-    .update(changes)
-    .then(() => findById(id));
-}
-
-function remove(id) {
-  return db("schemes").where("id", id).del();
-}
+const db = require('../data/db-config.js');
 
 module.exports = {
-  find,
-  findById,
-  findSteps,
-  add,
-  addStep,
-  update,
-  remove,
-};
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
+    addStep
+}
+
+function find(){
+ return db('schemes');
+}
+
+function findById(id){
+    return db('schemes')
+    .where({ id })
+    .first();
+}
+
+function findSteps(id){
+    return db("steps")
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where("scheme_id", id);
+}
+
+function add(scheme){
+    return db("schemes")
+    .insert(scheme, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function update(changes, id){
+    return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then(() =>{
+        return findById(id)
+    });
+}
+
+function remove(id){
+    return db("schemes")
+    .where("id", id)
+    .del();
+}
+
+function addStep(step, id) {
+    return db('steps')
+      .insert({ ...step, scheme_id: id });
+  };
